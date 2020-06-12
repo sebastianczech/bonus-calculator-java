7200e7ae9ac55133df24b8531692189dcafcd299
Sebastian Czech <sebaczech@gmail.com>
2020-06-12 06:39:00 +0200

    feature: Return no bonus if sales is below quota

--- src/main/java/tdd/bonus/BonusCalculator.java
+++ src/main/java/tdd/bonus/BonusCalculator.java
@@ -2,4 +2,7 @@ package tdd.bonus;
 
 public class BonusCalculator {
 
+    public Integer calculate(Integer sales, Integer quota) {
+        return 0;
+    }
 }
--- src/test/java/tdd/bonus/BonusCalculatorTest.java
+++ src/test/java/tdd/bonus/BonusCalculatorTest.java
@@ -7,7 +7,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class BonusCalculatorTest {
 
     @Test
-    public void startHere() {
-        assertThat(4).isEqualTo(5);
+    public void should_calculate_no_bonus_if_sales_is_below_quota() {
+        // given
+        BonusCalculator bonusCalculator = new BonusCalculator();
+
+        // when
+        Integer bonus = bonusCalculator.calculate(13000, 15000);
+
+        // then
+        assertThat(bonus).isEqualTo(0);
     }
+
 }
